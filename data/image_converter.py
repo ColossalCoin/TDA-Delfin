443539c1f65ff3f10f2826dcf6a6fa2873c47c35
ColossalCoin <abelcarvajal18@gmail.com>
2024-07-13 12:43:38 -0600

    Agregué el parámetro mode para elegir la escala de colores de las imagenes en la función get_images().

--- data/image_converter.py
+++ data/image_converter.py
@@ -5,14 +5,14 @@ from numpy import asarray
 
 current_path = getcwd()
 
-def image_to_array(path:Path, image_size:tuple):
+def image_to_array(path:Path, image_size:tuple, mode:str):
     image = Image.open(path)
     image = image.resize(image_size)
-    image = image.convert('L')
+    image = image.convert(mode)
     image_array = asarray(image)
     return image_array
     
-def get_images(image_size:tuple, augmented=False):
+def get_images(image_size:tuple, augmented=False, mode='L'):
     yes_arrays = []
     no_arrays = []
     
@@ -29,12 +29,12 @@ def get_images(image_size:tuple, augmented=False):
     for image in yes_images:
         if image != '.ipynb_checkpoints':
             image_path = Path(str(brain_tumor_yes) + r'\\' + image)
-            yes_arrays.append(image_to_array(image_path, image_size))
+            yes_arrays.append(image_to_array(image_path, image_size, mode))
         
     for image in no_images:
         if image != '.ipynb_checkpoints':
             image_path = Path(str(brain_tumor_no) + r'\\' + image)
-            no_arrays.append(image_to_array(image_path, image_size))
+            no_arrays.append(image_to_array(image_path, image_size, mode))
     
     yes_arrays = asarray(yes_arrays)
     no_arrays = asarray(no_arrays)
