3d4c278ae6faadfee468cf41e631fe93c2f2c6ac
ColossalCoin <abelcarvajal18@gmail.com>
2024-07-29 13:21:53 -0600

    Aguregué la función train_and_save que aplica el pipeline deseado y guarda los archivos según el parámetro original:bool

--- pipeline_processor.py
+++ pipeline_processor.py
@@ -2,14 +2,23 @@ from tda_pipeline import pipeline
 from data.image_converter import get_images
 import pickle
 
-yes, no = get_images((224, 224))
-
-yes_piped = pipeline(yes)
-no_piped = pipeline(no)
-
-with open('models/yes_piped.pickle', 'wb') as file:
-    pickle.dump(yes_piped, file)
-    file.close()
-with open('models/no_piped.pickle', 'wb') as file:
-    pickle.dump(no_piped, file)
-    file.close()
+def train_and_save(size=(224, 224), original=False):
+    yes, no = get_images(size)
+    yes_piped = pipeline(yes, original)
+    no_piped = pipeline(no, original)
+    
+    if not original:
+        yes_path = r'models/yes_piped.pickle'
+        no_path = r'models/no_piped.pickle'
+    else:
+        yes_path = r'models/yes_piped_original.pickle'
+        no_path = r'models/no_piped_original.pickle'
+        
+    with open(yes_path, 'wb') as file:
+        pickle.dump(yes_piped, file)
+        file.close()
+    with open(no_path, 'wb') as file:
+        pickle.dump(no_piped, file)
+        file.close()
+        
+train_and_save(size=(224, 224), original=True)
